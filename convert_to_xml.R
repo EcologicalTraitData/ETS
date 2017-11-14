d0a10d7eac39c306d2367847e115c1f13ac42ee1
Florian Schneider <florian.schneider@univ-montp2.fr>
2017-11-14 12:31:48 +0100

    add xml script and build xml

---
+++ convert_to_xml.R
@@ -0,0 +1,24 @@
+library(XML)
+
+convertToXML <- function(df,name)
+{
+  xml <- XML::xmlTree("Test")
+  xml$addNode(name, close=FALSE)
+  for (i in 1:nrow(df)) {
+    xml$addNode("value", close=FALSE)
+    for (j in names(df)) {
+      xml$addNode(j, df[i, j])
+    }
+    xml$closeTag()
+  }
+  xml$closeTag()
+  return(xml)
+}
+
+glossary <- read.csv("TraitDataStandard.csv")
+
+template = convertToXML(glossary,"ETS")
+XML::saveXML(template$value(), file = "TraitDataStandard.xml")
+
+
+# convert to owl / read ontologies using ontologyIndex
